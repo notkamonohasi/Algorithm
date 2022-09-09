3609cca48bf134cfd90c2d43a91dac2391f07628
notkamonohasi <82232581+notkamonohasi@users.noreply.github.com>
2022-09-09 23:57:58 +0900

    Create Eratosthenes.cpp

---
+++ Eratosthenes.cpp
@@ -0,0 +1,21 @@
+vector<bool> Eratosthenes(ll N) {
+    vector<bool> isprime(N + 1, true);
+    isprime[0] = isprime[1] = false;
+    for (ll p = 2; p <= N; ++p) {
+        if (!isprime[p]) continue;
+        for (int q = p * 2; q <= N; q += p) {
+            isprime[q] = false;
+        }
+    }
+    return isprime;
+}
+
+// nまでの素数が入ったvectorを作成
+vector<ll> makePrimeVec(ll n){
+    vector<bool> isprime = Eratosthenes(n);
+    vector<ll> ret;
+    rep(i, n){
+        if(isprime[i]) ret.push_back(i);
+    }
+    return ret;
+}
