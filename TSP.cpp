727ee0a99c05a9b1f70e0ab4237ffa7e1bb19ea3
notkamonohasi <82232581+notkamonohasi@users.noreply.github.com>
2022-10-23 14:16:10 +0900

    Update TSP.cpp

--- TSP.cpp
+++ TSP.cpp
@@ -1,17 +1,28 @@
 /* u->vが存在しない場合はcost[u][v]をLLONG_MAXで初期化しておく必要あり 
- * verify : https://atcoder.jp/contests/abc180/submissions/34526777 
+ * costの型はlong longかdoubleである必要がある
+ * verify : https://atcoder.jp/contests/abc180/submissions/35913642
  */
+template<typename T>
 struct TSP{
-    Mat cost, dp;
-    ll n;
-    TSP(Mat cost_) : cost(cost_), n(cost_.size()){}
+    using TMat = vector<vector<T>>;
+    TMat cost, dp;
+    ll n, m;
+    T INF;
+    TSP(TMat cost_) : cost(cost_), n(cost_.size()){
+        if(typeid(T) == typeid(double)) INF = DBL_MAX;
+        else if(typeid(T) == typeid(ll)) INF = LLONG_MAX;
+        else{
+            print("type error");
+            exit(EXIT_FAILURE);
+        }
+    }
     
-    ll getCost(ll start){
-        dp = init_mat(1 << n, n, -1);
+    T getCost(ll start){
+        dp = TMat(1 << n, vector<T>(n, -1));
         return getCostSub(0, start, start);
     }
     
-    ll getCostSub(ll visit, ll pos, ll start){
+    T getCostSub(ll visit, ll pos, ll start){
         if(dp[visit][pos] >= 0) return dp[visit][pos];
         else{
             if(visit == (1 << n) - 1 && pos == start){
@@ -19,19 +30,19 @@ struct TSP{
                 return dp[visit][pos];
             }
         }
-    
-        ll ret = LLONG_MAX;
+
+        T ret = INF;
         rep(i, n){
-            if(cost[pos][i] == LLONG_MAX || pos == i) continue;
+            if(cost[pos][i] == INF || pos == i) continue;
             if(!(visit >> i & 1)){ 
                 ll nextVisit = visit | 1 << i;
                 if(i == start && nextVisit != (1 << n) - 1) continue;   // ゴール状態でないのにスタート地点に戻ってくるのを防ぐ
-                ll nextCost = getCostSub(nextVisit, i, start);
-                if(nextCost != LLONG_MAX) ret = min(ret, nextCost + cost[pos][i]);
+                T nextCost = getCostSub(nextVisit, i, start);
+                if(nextCost != INF && cost[pos][i] != INF) ret = min(ret, nextCost + cost[pos][i]);
             }
         }
     
-        if(ret != LLONG_MAX) dp[visit][pos] = ret;   // 解決不可能なルート
+        if(ret != INF) dp[visit][pos] = ret;   // 解決不可能なルート
         return ret;
     }
 };
