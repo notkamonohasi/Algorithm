46854abeb63271f5a8ed8708035a0cc16c75c1fa
notkamonohasi <82232581+notkamonohasi@users.noreply.github.com>
2023-02-11 20:45:40 +0900

    Create CumulativeSum.cpp

---
+++ CumulativeSum.cpp
@@ -0,0 +1,23 @@
+/* 未verify
+ */
+
+template<typename T>
+struct CumulativeSum{
+    vector<T> v, sum; 
+    ll n;
+    
+    CumulativeSum(){}
+    CumulativeSum(vector<T>& v_) : v(v_){ init(); }
+    
+    void init(){
+        n = v.size(); 
+        sum.resize(n + 1, (T)0);
+        rep(i, n) sum[i + 1] = sum[i] + v[i];
+    }
+    
+    // [a, b)の和を出力
+    T query(ll a, ll b){ return sum[b] - sum[a]; }
+    
+    // [0, b)の和を出力
+    T query(ll b){ return sum[b]; }
+}; 
