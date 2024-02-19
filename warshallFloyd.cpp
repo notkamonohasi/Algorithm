4a80bc4b440d648e4d7ba34de55029db7d5f3335
notkamonohasi <82232581+notkamonohasi@users.noreply.github.com>
2024-02-20 00:08:08 +0900

    Update warshallFloyd.cpp

--- warshallFloyd.cpp
+++ warshallFloyd.cpp
@@ -1,8 +1,12 @@
-Mat warshallFloyd(Mat mat){
+// オーバーフロー対策を入れている
+// verified: https://atcoder.jp/contests/abc243/submissions/50456774
+Mat warshallFloyd(Mat mat, ll INF = LLONG_MAX){
     ll V = mat.size();
     rep(k, V){
         rep(i, V){
+            if(mat[i][k] == LLONG_MAX) continue;
             rep(j, V){
+                if(mat[k][j] == LLONG_MAX) continue;
                 mat[i][j] = min(mat[i][j], mat[i][k] + mat[k][j]);
             }
         }
